4c15a0c3b1522bb2ab1e7677c1f057262afb4c96
Prithvi raj <46382945+Prithvi-ra-j@users.noreply.github.com>
2026-09-25 12:04:11 +0530

    feat: load Geist typography from package

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import { ToastProvider } from './components/ui/ToastContext.jsx';
+import 'geist/font/sans.css';
+import 'geist/font/mono.css';
 import './index.css';
 
 import { ErrorBoundary } from './components/ErrorBoundary.jsx';
